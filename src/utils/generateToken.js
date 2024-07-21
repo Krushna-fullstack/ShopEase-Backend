4da06036b6f0640e24c1c94da43861becb052c41
Krushna <fullstack.kanha@gmail.com>
2024-07-21 21:00:02 +0530

    chore: Add generateToken utility function for JWT token generation

---
+++ src/utils/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export default generateToken;
