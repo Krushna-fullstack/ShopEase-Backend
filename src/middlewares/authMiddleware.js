44c73d85635e3a97e29bb96154a7d522ddbcf033
Krushna <fullstack.kanha@gmail.com>
2024-07-29 21:18:47 +0530

    feat: Add logout functionality and auth middleware
    This commit adds the logout functionality to the `logoutCurrentUser` route, allowing users to log out by clearing the JWT token from the cookie. It also introduces the `authMiddleware` file, which includes the `authenticate` and `authorizeAdmin` middleware functions for user authentication and authorization.

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler.js";
+import { User } from "../models/userModel.js";
+
+const authenicate = asyncHandler(async (req, res, next) => {
+  let token;
+
+  token = req.cookie.jwt;
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token found");
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decoded.userId).select("-password");
+
+  next();
+});
+
+const authorizeAdmin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send("Not authorized as an admin");
+  }
+});
+
+export { authenicate, authorizeAdmin };
